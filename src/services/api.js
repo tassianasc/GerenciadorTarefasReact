379fed924f82754908d5dbc780102602e7444f4a
tassianasc <118405272+tassianasc@users.noreply.github.com>
2024-12-10 16:57:02 -0300

    Delete src directory

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-//Configura o cliente Axios para comunicação com o backend.
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:5000', // Certifique-se de que o backend está rodando nesta porta
-});
-
-export default api;
